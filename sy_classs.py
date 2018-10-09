9a5127156d48d6c60a054885dc9c11c336ae4eac
DoubiLi <lipenghui315@gmail.com>
2018-10-09 20:34:55 +0800

    finish phi function

--- sy_classs.py
+++ sy_classs.py
@@ -5,20 +5,16 @@
 '''
 from z3 import *
 import unicodedata as uni
+import os
 
-# class SEFunction: which can be put off since now we only focus on single function
-# TODO set a class to it
 # TODO Other nodes that have to be handled
-# TODO United Nodes and function 'Phi'
 # TODO Identify LOOPs(Path goes from smaller node to larger one??)
-# TODO Support user input identifycation, which should be in Array_Fetch
-#      But how to mark it when analysize the loop condition?
+# TODO Support user input identifycation, which should be in Array_Fetch #      But how to mark it when analysize the loop condition?
 # TODO Where to start???
 # TODO Identify which path is reachable there.
 
 class ParseFunc(object):
-    ReachNode = []
-    NodePath = []
+    BlockPath = []
     CstList = []
     Var = []
     Parents = []
@@ -28,23 +24,32 @@ class ParseFunc(object):
         Blocks = script.split('\nBlock#')
         for i in range(len(Blocks)):
             self.block.append(Blocks[i].split('\n'))
+            self.BlockPath.append([])
+            self.Parents.append([])
+            s = Solver()
+            self.CstList.append(s)
         return
 
     def TravelBlock(self, nblock):
+        self.BlockPath[nblock].append(nblock)
+        #print('path',nblock, self.BlockPath[nblock])
         block = self.block[nblock]
-        for j in range(len(block)):
+        j = 1
+        while j < len(block):
             # handle types with corresponding functions specified
             block[j] = block[j].strip() 
             if block[j].isdigit():
                 print('Begin to travel block', block[j])
             elif block[j].find('Parent') != -1:
-                abf = 1
+                self.Parents[nblock].append(int(block[j][14:]))
                 #self.Parents[nblock].append(self.Parent(block[j][8:])) # nothing with parent, not deal with it at this moment.
             elif block[j] == 'Expr_Assign':
                 self.Expr_Assign(block[j + 1].strip()[5:], block[j + 2].strip()[6:], block[j + 3].strip()[8:])
                 j += 3
             elif block[j] == 'Stmt_Jump': 
-                self.Stmt_Jump(block[j+1].strip()[8:], nblock) # there may be a loop
+                self.CstList[int(block[j+1].strip()[14:])].add(self.CstList[nblock].assertions())
+                self.BlockPath[int(block[j+1].strip()[14:])] = self.BlockPath[nblock][:]
+                self.TravelBlock(int(block[j+1].strip()[14:])) # there may be a loop
                 j += 1
             elif block[j] == 'Terminal_Return':
                 self.Terminal_Return(block[j+1].strip()[6:])
@@ -59,38 +64,41 @@ class ParseFunc(object):
                 tmp = block[j+1].strip()[6:]
                 If_Cst = self.JumpIfCond(tmp)
                 If_block_split = block[j+2].split('Block#')
-                #self.CstList[int(If_block_split[1])].add(If_Cst)
-                self.TravelBlock(int(If_block_split[1]))
+                self.CstList[int(If_block_split[1])].add(self.CstList[nblock].assertions())
+                self.CstList[int(If_block_split[1])].add(If_Cst) #need to add current cst
                 Else_block_split = block[j+3].split('Block#')
-                #self.CstList[int(Else_block_split[1])].add(Not(If_Cst))  
-                self.TravelBlock(int(Else_block_split[1]))
+                self.CstList[int(Else_block_split[1])].add(self.CstList[nblock].assertions())
+                self.CstList[int(Else_block_split[1])].add(Not(If_Cst)) #need to add current cst
+                ###########
+                self.BlockPath[int(If_block_split[1])] = self.BlockPath[nblock][:]
+                self.TravelBlock(int(If_block_split[1]))
+
+                '''
+                pid = os.fork()
+                if pid == 0:
+                    self.BlockPath[int(If_block_split[1])] = self.BlockPath[nblock][:]
+                    self.TravelBlock(int(If_block_split[1]))
+                else:
+                    self.BlockPath[int(Else_block_split[1])] = self.BlockPath[nblock][:]
+                    self.TravelBlock(int(Else_block_split[1]))
+                '''
                 j += 3
             elif block[j] == 'Expr_BinaryOp_Plus':
                 self.Expr_BinaryOp_Plus(block[j + 1].strip()[6:], block[j + 2].strip()[7:], block[j + 3].strip()[8:])
                 j += 3
             elif block[j].find('Phi') != -1:
-                a = 1 # make space
-                #CstList[i].add(Phi(block[j], i)) # deal with all stuff with function phi
-                # this function may be complex since it should combine the constraints together to the next variable
-                # something need to do here
+                self.Phi(block[j].strip())# deal with all stuff with function phi
             else:
                 print("ERROR: unknown expr/stmt")
-                print(block[j])
-    '''
-        for i in range(len(Blocks)):
-            print('Block', i)
-            print(CstList[i])
-            print(CstList[i].check())
-            if(CstList[i].check()):
-                ReachNode[i] = True
-                print(CstList[i].model())
-    '''
+                #print(block[j])
+            j +=1
 
     '''
     @ function: this is to deal with array fetch in php
     '''
     def Expr_ArrayDimFetch(self, var, dim, result):
-        return
+        return var[dim]
+
     '''
     @function:      Find the parental node
     @param pblock:  Parent block
@@ -99,16 +107,6 @@ class ParseFunc(object):
     def Parent(self, pblock):
         return int(pblock[6:])
 
-    # TODO
-    '''
-    Generally, Stmt_Jump happens when there is a loop...
-    '''
-    def Stmt_Jump(self, target, nblock):
-        target = int(target[6:])
-        #if(target in self.NodePath[nblock]):
-        #    self.NodePath[nblock].append(target)
-        #    print('There is a LOOP:', self.NodePath) # need to mark
-        return
     '''
     @ function Phi is the joining point in the control flow graph.
                It requires to handle more than two inputs from its
@@ -118,45 +116,33 @@ class ParseFunc(object):
     it needs to write the result in the CstList and Varaible
     '''
     # TODO
-    def Phi(self, expr, nblock):
+    def Phi(self, expr):
+        nvar = expr.split('Var#')
+        r1 = nvar[1].find('<') 
+        r2 = nvar[1].find('=')
+        kk = 0
+        if r1 == -1:
+            kk = r2
+        else: 
+            kk = r1 
+        reidx = int(nvar[1][0:kk])
+        self.Assign_Element(self.Var, reidx, '')
+        i = 1
+        while i < len(nvar):
+            rt1 = nvar[i].find('<')
+            rt2 = len(nvar[i])
+            if rt1 == -1:
+                kk = rt2 -2
+            else:
+                kk = rt1
+            rtidx = int(nvar[i][0:kk])
+            if type(self.Var[rtidx]) != str: # here needsed to assin value to new var, but cannot 
+                                             # compare str with symbolic expressions
+                self.Var[reidx] = self.Var[rtidx]
+                return
+            i +=1
+        print('ERROR: PHI Var[%d] not initialized', reidx)
         return
-        '''
-        r1 = expr.find('<') 
-        r2 = expr.find('=')
-        r = min(r1, r2-1) # there is a space for r2
-        reidx = int(expr[4:r])
-        left = expr[r2+6: expr.find(',')]
-        #print('left', left)
-        right = expr[expr.find(',') + 2 : ]
-        #print('right', right)
-        lvalue = 'error'
-        rvalue = 'error'
-        if(left.find('Var') != -1):
-            lidx = left.find('<') 
-            if(lidx == -1):
-                lidx = len(left) 
-            lvalue = Var[int(left[4:lidx])]
-        elif(left.find('LITERAL')!= -1):
-            lidx = left.find(')')
-            lvalue = int(left[8:lidx])
-
-        if(right.find('Var') != -1):
-            ridx = right.find('<') 
-            if(ridx == -1):
-                ridx = len(right) 
-            rvalue = Var[int(right[4:ridx])]
-        elif(right.find('LITERAL')!= -1):
-            ridx = right.find(')')
-            rvalue = int(right[8:ridx])
-
-        cst = False
-        for i in range(len(Parents[nblock])):
-            tcst = True
-            for c in CstList[Parents[nblock][i]].assertions():
-                tcst = And(tcst, c)
-            cst = Or(cst,tcst)
-        return cst
-        '''
 
     '''
     @function:      return expression or value
@@ -205,10 +191,7 @@ class ParseFunc(object):
             reidx = len(result)
 
         n = int(result[4:reidx])
-        #print(left, right, result)
-        #print(lidx, ridx, reidx)
         new_Cst = lvalue + rvalue
-        #print('new_Cst',new_Cst)
         self.Assign_Element(self.Var, n, new_Cst)
         #self.Var[n]= new_Cst
         return
@@ -222,7 +205,7 @@ class ParseFunc(object):
         i = len(List)
         n = index
         while i < n:
-            List.append(' ')
+            List.append('')
             i += 1
         List.append(value)
         return
@@ -249,7 +232,6 @@ class ParseFunc(object):
         while i < n + 1 :
             self.Var.append(Real('Var' + str(i)))
             i += 1
-        #print(expr, evalue)
         self.Assign_Element(self.Var, int(var[4:vidx]),evalue)
         self.Assign_Element(self.Var, int(result[4: reidx]), evalue) # result is equal to var
         return
@@ -259,8 +241,6 @@ class ParseFunc(object):
         idx = var.find('<')
         if(idx == -1):
             idx = len(var)
-        print(idx, '\n')
-        print(var)
         n = int(var[4:idx])
         self.Assign_Element(self.Var, n, Real('Var'+str(n)))
         return
@@ -292,10 +272,7 @@ class ParseFunc(object):
             reidx = len(result)
         n = int(result[4:reidx])
 
-        #print(left, right, result)
-        #print(lidx, ridx, reidx)
         new_Cst = lvalue < rvalue
-        #print('new_Cst',new_Cst)
         #self.Var[int(result[4: reidx])]= new_Cst
         self.Assign_Element(self.Var, n, new_Cst)
         return
@@ -313,7 +290,6 @@ class ParseFunc(object):
             idx = len(cond)
         Var_idx = int(cond[4:idx])
         new_Cst = self.Var[Var_idx]
-        #print(new_Cst, type(new_Cst))
         return new_Cst 
 
 
@@ -321,5 +297,9 @@ class ParseFunc(object):
 script = open('file.txt','r')
 script = script.read()
 start = ParseFunc(script)
-start.TravelBlock(0)
 start.TravelBlock(1)
+for i in range(len(start.BlockPath)):
+    print(i, ':')
+    print('BlockPath:', start.BlockPath[i])
+    print('CstList:', start.CstList[i])
+
